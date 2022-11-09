a4f33b56f4f458fb64f927df21dc2b76068c7e47
Chris Hetherton <78489707+itanglishdev@users.noreply.github.com>
2022-11-09 19:10:13 +0000

    added variables

--- main.py
+++ main.py
@@ -1,21 +1,25 @@
 #Fundamental Data types
 # int and float
 
-  print(type(2 + 4))
-  print(type(2 - 4))
-  print(type(2 * 4))
-  print(type(2 / 4))
+#print(type(2 + 4))
+#print(type(2 - 4))
+#print(type(2 * 4))
+#print(type(2 / 4))
   
-  print(type(0.0001))
-  print(type(0))
+#print(type(0.0001))
+#print(type(0))
   
-  print(type(20+1.1))
-  print(type(9.9+0.1))
+#print(type(20+1.1))
+#print(type(9.9+0.1))
 
-print(2 ** 3)
-print(5 // 4)
-print(6 % 4)
+#print(2 ** 3)
+#print(5 // 4)
+#print(6 % 4)
 
 # math function
-print(round(3.1))
-print(abs(-20))
+#print(round(3.1))
+#print(abs(-20))
+#print(bin(5))
+#print(int('0b101',2))
+iq = 190
+print('user',iq)
